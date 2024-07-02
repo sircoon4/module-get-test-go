f8c7830f244c3a5caff2f0c0e3eedd4eb595f22d
Kang Seokhun <kangseokhun43@gmail.com>
2024-07-02 15:24:31 +0900

    Adding bencodex-go module

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/sircoon4/module-get-test-go
 go 1.22.4
 
 require github.com/sircoon4/module-publish-test-go v0.1.1
+
+require github.com/sircoon4/bencodex-go v0.1.0 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/sircoon4/bencodex-go"
 	"github.com/sircoon4/module-publish-test-go/test"
 	"github.com/sircoon4/module-publish-test-go/testother"
 )
@@ -10,4 +11,16 @@ import (
 func main() {
 	fmt.Println(test.Test())
 	fmt.Println(testother.TestOther())
+
+	mapData := map[string]any{"a": 1, "b": "2", "c": 3}
+	encoded, err := bencodex.Encode(mapData)
+	if err != nil {
+		fmt.Println("error while encoding: ", err)
+	}
+	fmt.Println(encoded)
+	decoded, err := bencodex.Decode(encoded)
+	if err != nil {
+		fmt.Println("error while decoding: ", err)
+	}
+	fmt.Printf("Value: %v Type: %T\n", decoded, decoded)
 }
